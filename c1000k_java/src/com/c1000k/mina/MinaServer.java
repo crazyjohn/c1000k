8a555c53f6ea91cad8a359bcc18b2ff6f07c1b95
crazyjohn <magicstoneljg@163.com>
2016-02-25 14:22:36 +0800

    c1000k java
    c1000k java

---
+++ c1000k_java/src/com/c1000k/mina/MinaServer.java
@@ -0,0 +1,10 @@
+package com.c1000k.mina;
+
+public class MinaServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ c1000k_java/src/com/c1000k/netty/NettyServer.java
@@ -0,0 +1,35 @@
+package com.c1000k.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.example.http.websocketx.benchmarkserver.WebSocketServerInitializer;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class NettyServer {
+
+	public static void main(String[] args) throws InterruptedException {
+		final int PORT = 8881;
+		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap boot = new ServerBootstrap();
+			boot.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.handler(new LoggingHandler(LogLevel.INFO))
+					.childHandler(new WebSocketServerInitializer(null));
+			Channel channel = boot.bind(PORT).sync().channel();
+			System.out
+					.println("Open your web brower and navigate to http://127.0.0.1:"
+							+ PORT + "/");
+			channel.closeFuture().sync();
+		} finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
+
+}
