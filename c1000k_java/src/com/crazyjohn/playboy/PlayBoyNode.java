d64dcce9628a5f5f3d697d8b0a6ebdc109eed811
crazyjohn <magicstoneljg@163.com>
2016-03-09 15:41:17 +0800

    scale mode
    scale mode

--- c1000k_java/src/com/crazyjohn/playboy/PlayBoyNode.java
+++ c1000k_java/src/com/crazyjohn/playboy/PlayBoyNode.java
@@ -1,14 +1,8 @@
 package com.crazyjohn.playboy;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerOptions;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
 
-import com.crazyjohn.playboy.log.Logger;
-import com.crazyjohn.playboy.logic.self.Self;
-import com.crazyjohn.playboy.logic.test.Test;
+import com.crazyjohn.playboy.scale.ScaleMode;
 
 /**
  * The playboy node;
@@ -20,38 +14,6 @@ import com.crazyjohn.playboy.logic.test.Test;
  */
 public class PlayBoyNode {
 
-	protected static void singleMode(Vertx vertx, int port) {
-		scaleOutMode(vertx, 1, port);
-	}
-
-	protected static void scaleOutMode(Vertx vertx, int instanceCount, int port) {
-		for (int i = 0; i < instanceCount; i++) {
-			HttpServerOptions options = new HttpServerOptions();
-			HttpServer server = vertx.createHttpServer(options);
-			// create router
-			Router router = Router.router(vertx);
-			// test case
-			Test testController = new Test();
-			router.route("/hi/").handler(testController::sayHi);
-			// upload case
-			Self selfController = new Self();
-			// Enable multipart form data parsing
-			Logger.log(String.format("Upload dir: %s", "/upload"));
-		    router.route().handler(BodyHandler.create().setUploadsDirectory("/upload"));
-			// form
-			router.route("/uploadForm/").handler(selfController::uploadForm);
-			router.route("/form/").handler(selfController::form);
-			// start server
-			server.requestHandler(router::accept).listen(port, result -> {
-				if (result.succeeded()) {
-					Logger.log("The playboy ready!");
-				} else {
-					Logger.log("Some fucking error???: " + result.cause());
-				}
-			});
-		}
-	}
-
 	/**
 	 * entrance
 	 * 
@@ -60,9 +22,10 @@ public class PlayBoyNode {
 	public static void main(String[] args) {
 		// create vertx instance
 		Vertx vertx = Vertx.vertx();
-		// scale mode
+		// processor count
 		int processorCount = 1;
-		scaleOutMode(vertx, processorCount, 8082);
+		// choose scale mode
+		ScaleMode.scaleOutMode(vertx, processorCount, 8080);
 	}
 
 }
